480ae309123b8883ec077515a4851b1082e6a1c8
陈达寅 <11612512@mail.sustc.edu.cn>
2020-02-11 11:40:17 -0800

    Update queue_test.cc

--- queue_test.cc
+++ queue_test.cc
@@ -3,8 +3,6 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <bits/stdc++.h>
-#include <map.h>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -12,8 +10,8 @@ using namespace std;
 int
 main (int argc, char* argv[])
 {
-	int n = stoi(argv[1], 0, 10);
-	int t = stoi(argv[2], 0, 10);
+	int n = stoi(argv[1]);
+	int t = stoi(argv[2]);
 	cout<<"n="<<n<<endl;
 	cout<<"t="<<t<<endl;
 }
